d7c30eaa702901b0b605b9b26b2fa9e3dbde6a08
ivkudryavtsev <ivkudryavtsev@skr.local>
2023-01-26 16:53:37 +0300

    20220126 16-53

--- 02_js_basic/app.js
+++ 02_js_basic/app.js
@@ -1,13 +1,24 @@
-
-
-function censer(){
-    let a=0;
-    console.log(a);
+const mainobject= {
+    raplaces :[],
+    counter :0,
+}
+function censer(a,b=0){
+    if(b !== 0) mainobject.raplaces.push([a,b])
+    else 
+    mainobject.raplaces.forEach(item=>{
+            console.log(item[0],item[1]);
+            a=a.replaceAll(item[0],item[1]);
+            console.log(a);
+        }
+    );
+    return 1;
+    // return a;
 };
 
 const changeScene = censer;
 
-changeScene();
-
-changeScene();
-changeScene();
+changeScene('1','2');
+changeScene('3','4');
+changeScene('5','6');
+console.log('->>',mainobject.raplaces)
+console.log(changeScene('51231223   5423512313123123'));
