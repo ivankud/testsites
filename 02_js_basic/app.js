eeda0eebc260b701d1982dac12f86b13bf3ffbef
ivkudryavtsev <ivkudryavtsev@skr.local>
2023-01-27 15:55:22 +0300

    20230127

--- 02_js_basic/app.js
+++ 02_js_basic/app.js
@@ -5,20 +5,16 @@ const mainobject= {
 function censer(a,b=0){
     if(b !== 0) mainobject.raplaces.push([a,b])
     else 
-    mainobject.raplaces.forEach(item=>{
-            console.log(item[0],item[1]);
-            a=a.replaceAll(item[0],item[1]);
-            console.log(a);
-        }
-    );
-    return 1;
-    // return a;
+    mainobject.raplaces.forEach(item=>a=a.replaceAll(item[0],item[1]));
+    // return 1;
+    return a;
 };
 
 const changeScene = censer;
 
 changeScene('1','2');
-changeScene('3','4');
+changeScene('.','4');
 changeScene('5','6');
-console.log('->>',mainobject.raplaces)
-console.log(changeScene('51231223   5423512313123123'));
+// console.log('->>',mainobject.raplaces)
+console.log('5.....1231223   5423512313123123..')
+console.log(changeScene('5.....1231223   5423512313123123..'));
